7c8f19876509e1b33aec2fa18f384fa3c4110e2a
TheBigR <nevoroy@gmail.com>
2020-10-28 14:07:42 +0200

    initial commit

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Accordion from './Accordion';
+
+
+const App  = () => {  
+
+return (
+        <div>
+            <Accordion />
+        </div>
+    );
+};
+
+export default App
